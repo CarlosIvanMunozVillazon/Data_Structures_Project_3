bea33a1c60604d78107cda6d2c4a734c2d99f74b
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2023-05-28 15:17:20 -0500

    interface folder moved

--- app/interface/Product.interface.ts
+++ app/interface/Product.interface.ts
@@ -1,6 +1,16 @@
 export interface ProductInterface {
-    titulo: string,
-    precio : string,
-    link : string,
-    tienda : string
+  titulo: string;
+  precio: string;
+  link: string;
+  tienda: string;
 }
+
+//New product interface
+// export interface ProductInterface {
+//   title: string;
+//   price: number;
+//   lint: string;
+//   store: string;
+//   image: string;
+//   brand: string;
+// }
