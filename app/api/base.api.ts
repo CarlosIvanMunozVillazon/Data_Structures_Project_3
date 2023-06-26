3375180edcb7938bf684fcd8f7463f197c1cd81e
Javier Tarazona <96489034+JavierTarazona06@users.noreply.github.com>
2023-06-25 19:55:19 -0500

    Local

--- app/api/base.api.ts
+++ app/api/base.api.ts
@@ -1,13 +1,21 @@
 import axios from "axios";  //import axios library for making petitions
 
 //Copy the base API URL for hitting its endpoints.
-export const BASE_URL = "http://127.0.0.1:8000";
+export const BASE_URL_LOCAL = "http://127.0.0.1:8000";
+
+export const BASE_URL = "http://18.228.19.131/";
 
 //Instantiate Axios and then export it.
 //e.i Axios's instance means API connection is created.
 export const instance = axios.create(
     {
         baseURL:BASE_URL
+    }
+)
+
+export const instance_local = axios.create(
+    {
+        baseURL:BASE_URL_LOCAL
     }
 )
 
